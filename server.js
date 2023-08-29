d2b1289dc0280a6a97f1fa68d1f7bf74da519a03
Yusuf <yusuftaha9@gmail.com>
2023-08-29 14:31:53 +0300

    smaller server code

--- server.js
+++ server.js
@@ -5,23 +5,7 @@ const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 const FOLDER = process.env.FOLDER || "./db";
-
 if (!fs.existsSync(FOLDER)) fs.mkdirSync(FOLDER);
-
-const headers = {
-  "Access-Control-Allow-Origin": "*",
-};
-
-const server = http.createServer((req, res) => {
-  if (req.method === "POST") {
-    handlePutRequest(req, res);
-  } else if (req.method === "GET") {
-    handleGetRequest(req, res);
-  } else {
-    res.writeHead(405, { "Content-Type": "text/plain", ...headers });
-    res.end("Method Not Allowed");
-  }
-});
 
 function hash(data) {
   const hash = crypto.createHash("sha1");
@@ -29,52 +13,42 @@ function hash(data) {
   return hash.digest("hex");
 }
 
-function handlePutRequest(req, res) {
-  let requestBody = "";
+const server = http.createServer((req, res) => {
+  const end = (code, body) => {
+    res.writeHead(code, { "Access-Control-Allow-Origin": "*" });
+    res.end(body);
+  };
 
-  req.on("data", (chunk) => {
-    requestBody += chunk;
-  });
+  if (req.method === "POST") {
+    let requestBody = "";
 
-  req.on("end", () => {
-    const fileHash = hash(requestBody);
-    const pathName = path.join(FOLDER, `${fileHash}.txt`);
-
-    console.log(`PUT ${pathName}`);
-    fs.writeFile(pathName, requestBody, (err) => {
-      if (err) {
-        res.writeHead(500, { "Content-Type": "text/plain", ...headers });
-        res.end("Internal Server Error");
-      } else {
-        res.writeHead(201, { "Content-Type": "text/plain", ...headers });
-        res.end("File Created");
-      }
+    req.on("data", (chunk) => {
+      requestBody += chunk;
     });
-  });
-}
 
-function handleGetRequest(req, res) {
-  const hash = req.url.slice(1);
-  // Check if it is base64
-  if (!hash.match(/^[0-9a-f]+$/)) {
-    res.writeHead(400, { "Content-Type": "text/plain", ...headers });
-    res.end("Invalid Request");
-    return;
-  }
+    req.on("end", () => {
+      const pathName = path.join(FOLDER, `${hash(requestBody)}.txt`);
 
-  const pathName = path.join(FOLDER, `${hash}.txt`);
-  console.log(`GET ${pathName}`);
+      console.log(`POST ${pathName}`);
+      fs.writeFile(pathName, requestBody, (err) => {
+        if (err) return end(500);
+        end(200);
+      });
+    });
+  } else if (req.method === "GET") {
+    const hash = req.url.slice(1);
+    // Prevents directory traversal
+    if (!hash.match(/^[0-9a-f]+$/)) return end(404);
 
-  fs.readFile(pathName, "utf8", (err, data) => {
-    if (err) {
-      res.writeHead(404, { "Content-Type": "text/plain", ...headers });
-      res.end("File Not Found");
-    } else {
-      res.writeHead(200, { "Content-Type": "text/plain", ...headers });
-      res.end(data);
-    }
-  });
-}
+    const pathName = path.join(FOLDER, `${hash}.txt`);
+    console.log(`GET ${pathName}`);
+
+    fs.readFile(pathName, "utf8", (err, data) => {
+      if (err) return end(404);
+      end(200, data);
+    });
+  } else end(405);
+});
 
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
